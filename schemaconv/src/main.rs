ffc65cadb6e6830382f786a6f12ecd6761cd5748
Eric Kidd <git@randomhacks.net>
2018-10-05 09:36:59 -0400

    Accept schema JSON as input
    This will allow using `schemaconv` in pipelines.

--- schemaconv/src/main.rs
+++ schemaconv/src/main.rs
@@ -4,6 +4,8 @@
 extern crate common_failures;
 extern crate env_logger;
 #[macro_use]
+extern crate failure;
+#[macro_use]
 extern crate log;
 extern crate schemaconvlib;
 extern crate serde;
@@ -16,11 +18,12 @@ extern crate strum_macros;
 extern crate url;
 
 use common_failures::Result;
+use failure::ResultExt;
 use schemaconvlib::drivers::{
     bigquery::BigQueryDriver,
     postgres::PostgresDriver,
 };
-use std::io::{stdout, Write};
+use std::io::{stdin, stdout, Write};
 use structopt::StructOpt;
 use url::Url;
 
@@ -38,11 +41,9 @@ enum OutputFormat {
 #[derive(Debug, StructOpt)]
 #[structopt(name = "schemaconv", about = "Convert between schema formats.")]
 struct Opt {
-    /// The URL of the database.
-    url: Url,
-
-    /// The name of the table for which to fetch a schema.
-    table_name: String,
+    /// The URL of the database, with the table name specified as a `#`
+    /// fragment. If this URL is omitted, read a JSON table schema from stdin.
+    url: Option<Url>,
 
     /// The output format to use.
     #[structopt(short = "O", default_value = "json")]
@@ -56,10 +57,23 @@ fn run() -> Result<()> {
     let opt = Opt::from_args();
     debug!("{:?}", opt);
 
-    let table = PostgresDriver::fetch_from_url(&opt.url, &opt.table_name)?;
+    // If we have a database URL, read our schema from the database.
+    let table = if let Some(url) = &opt.url {
+        let mut base_url = url.clone();
+        base_url.set_fragment(None);
+        let table_name = url.fragment().ok_or_else(|| {
+            format_err!("Database URL must include table name after `#`")
+        })?;
+        PostgresDriver::fetch_from_url(&base_url, &table_name)?
+    } else {
+        let stdin = stdin();
+        let mut input = stdin.lock();
+        serde_json::from_reader(&mut input)
+            .context("error reading from stdin")?
+    };
+
     let stdout = stdout();
     let mut out = stdout.lock();
-
     match opt.output_format {
         OutputFormat::Json => {
             serde_json::to_writer_pretty(&mut out, &table)?;
