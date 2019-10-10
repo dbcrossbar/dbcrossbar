5b5889d6e09eec727547683f757a4acc51275287
Eric Kidd <git@randomhacks.net>
2019-10-10 10:23:56 -0400

    Text fix: Fix upsert failure using tricky column names
    We've been running the tests all along, so I'm not sure how this made it
    onto `master`. But yes, we do need `NOT NULL` on our key column to
    insert into BigQuery.

--- dbcrossbar/fixtures/tricky_column_names.sql
+++ dbcrossbar/fixtures/tricky_column_names.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tricky_column_names (
-  "person__Delivery Zone 4.14" text,
+  "person__Delivery Zone 4.14" text NOT NULL,
   "person__$Region - 8/1" text,
   "person__Do you have any allergies?" text,
   "name" text,
