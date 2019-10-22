86a959473ae7312e88778fa6da9d12ab1d8461c9
Seamus Abshere <seamus@abshere.net>
2019-10-22 18:48:43 -0400

    remove upsert check again
    requiring destination tables to have REQUIRED set prevents solutions like `where upsert cols are not null`, because the temp table gets created with REQUIRED.

--- dbcrossbarlib/src/drivers/bigquery_shared/table.rs
+++ dbcrossbarlib/src/drivers/bigquery_shared/table.rs
@@ -135,18 +135,6 @@ impl BqTable {
             })
             .collect::<Result<Vec<&BqColumn>>>()?;
 
-        // As discussed at https://github.com/faradayio/dbcrossbar/issues/43,
-        // it's not obvious how to `MERGE` on columns that might be `NULL`.
-        // Until we have a solution that we like, fail with an error.
-        for merge_key in &merge_keys {
-            if merge_key.mode != Mode::Required {
-                return Err(format_err!(
-                    "BigQuery cannot upsert on {:?} because it is not REQUIRED (aka NOT NULL)",
-                    merge_key.name,
-                ));
-            }
-        }
-
         // Build a table when we can check for merge keys by name.
         let merge_key_table = merge_keys
             .iter()
