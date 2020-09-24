a3e8861f3c0d45c27e5bef3e4b8df6f4d882935b
Seamus Abshere <seamus@abshere.net>
2020-09-23 20:53:17 -0400

    postgres: support array of varchar

--- dbcrossbarlib/src/drivers/postgres_shared/catalog.rs
+++ dbcrossbarlib/src/drivers/postgres_shared/catalog.rs
@@ -206,6 +206,7 @@ fn pg_data_type(
             "_timestamp" => PgScalarDataType::TimestampWithoutTimeZone,
             "_timestamptz" => PgScalarDataType::TimestampWithTimeZone,
             "_uuid" => PgScalarDataType::Uuid,
+            "_varchar" => PgScalarDataType::Text,
             _ => return Err(format_err!("unknown array element {:?}", udt_name)),
         };
         Ok(PgDataType::Array {
@@ -334,6 +335,10 @@ fn parsing_pg_data_type() {
         ),
         (
             ("ARRAY", "pg_catalog", "_text"),
+            array(PgScalarDataType::Text),
+        ),
+        (
+            ("ARRAY", "pg_catalog", "_varchar"),
             array(PgScalarDataType::Text),
         ),
         (
